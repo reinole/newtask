ca94ec78c14c167b4ddf35ad80c9a2100bae7012
reinole <ole.christian.reinhartsen@trumf.no>
2024-03-13 11:55:21 +0100

    Dynamic pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,25 @@
 
 export const Pagination = ({ selectedPage, setSelectedPage, arrayLength }) => {
+
+    const countPages = (pages) => {
+        const numberOfPages = []
+
+        for (let i = 0; i < pages; i++) {
+            numberOfPages.push(i)
+        }
+
+        return numberOfPages
+    }
+
+    const pageNumber = countPages(arrayLength)
+
     return (
         <>
             <div>
                 {selectedPage > 0 && <button onClick={() => setSelectedPage(selectedPage - 1)}>{"<"}</button>}
+                {pageNumber.map(item => (
+                    <button key={item} onClick={() => setSelectedPage(item)}>{item + 1}</button>
+                ))}
                 {selectedPage < arrayLength - 1 && <button onClick={() => setSelectedPage(selectedPage + 1)}>{">"}</button>}
             </div>
         </>
